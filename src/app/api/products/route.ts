c41c05aea8f6d193442a922ce9064efc817890d3
jluo <jluo8@wgu.edu>
2024-10-14 22:39:50 -0700

    products

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -3,9 +3,13 @@ import { Products } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
   const id = 1;
   const name = `CuteCat-${Math.ceil(Math.random() * 999)}`;
+  // If no ID or name is provided, return a bad request response
+  if (!id || !name) {
+    return NextResponse.json({ error: "Missing id or name" }, { status: 400 });
+  }
 
   try {
     const result = await db.update(Products).set({ name }).where(eq(Products.id, id));
@@ -14,5 +18,16 @@ export async function POST(req: NextRequest, res: NextResponse) {
   } catch (error) {
     console.error("Error updating DB record:", error);
     return NextResponse.json({ error: "Error updating DB record" }, { status: 500 });
+  }
+}
+
+export async function GET(req: NextRequest) {
+  try {
+    const result = await db.select().from(Products);
+
+    return NextResponse.json(result);
+  } catch (error) {
+    console.error("Error fetching DB record:", error);
+    return NextResponse.json({ error: "Error fetching DB record" }, { status: 500 });
   }
 }
